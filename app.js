7be6f0b667b64c509c16376fa01d11a61aabeb92
Atharvagupta12 <atharvguptaofficial@gmail.com>
2023-07-08 22:10:30 +0530

    major updates , all files added

--- app.js
+++ app.js
@@ -16,3 +16,19 @@ else{
 }
 
 }
+
+
+// dark mode 
+
+
+var icon = document.getElementById("icon");
+
+    icon.onclick = function(){
+      document.body.classList.toggle("dark-theme");
+      if(document.body.classList.contains("dark-theme")){
+        icon.src = "Assets/sun.png";  
+      }else{
+        icon.src = "Assets/moon.png";
+      }
+    }
+
