463c75e2ec5b4e1b6bfcee7d66638cc3a7462daf
Atharvagupta12 <atharvguptaofficial@gmail.com>
2023-07-08 15:05:57 +0530

    adding home

---
+++ app.js
@@ -0,0 +1,18 @@
+// like button
+
+function Toggle(div){
+ 
+    
+   
+
+var btn = document.getElementById('div.id');
+if(div.classList.contains("fa-regular")){
+    div.classList.remove("fa-regular");
+    div.classList.add("fas");
+}
+else{
+   div.classList.remove("fas");
+    div.classList.add("fa-regular");
+}
+
+}
